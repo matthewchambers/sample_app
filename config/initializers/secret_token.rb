ef6afdf14cc7cf0274062e2be8fedf9ffd2cf31e
Matt Chambers <mchambers@skookum.com>
2014-01-27 09:12:29 -0500

    update security

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,5 +8,20 @@
 # You can use `rake secret` to generate a secure secret key.
 
 # Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'ecf93bd8b1d7e55c8de641bd6f1e4866f75bc461105c3743e39cb6b0ee5cc91177178d4e0db85d0ed687d1174aa381d4554ac4dc4fd391cc0705980ab23c37c1'
+# if you're sharing your code publicly
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
